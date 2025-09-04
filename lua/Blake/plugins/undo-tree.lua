815a64d935490e9dd2ce6d656db540b1b4c0deb9
blake-tron <akashbhaduri50@gmail.com>
2025-09-04 21:02:52 +0530

    optimizing

--- lua/Blake/plugins/undo-tree.lua
+++ lua/Blake/plugins/undo-tree.lua
@@ -1,6 +1,11 @@
 return {
-    "mbbill/undotree",
-    config = function()
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-    end,
+	"mbbill/undotree",
+	config = function()
+		vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+		vim.g.undotree_WindowLayout = 2
+		vim.g.undotree_SplitWidth = 40
+		vim.g.undotree_DiffpanelHeight = 10
+		vim.g.undotree_SetFocusWhenToggle = 1
+	end,
 }
